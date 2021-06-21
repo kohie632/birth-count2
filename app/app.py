c85115238328b762a5eba9feb348584b5353143d
chikoau <chikoau@gmail.com>
2021-06-22 02:36:48 +0900

    bug

--- app/app.py
+++ app/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask,render_template,request
 
 app = Flask(__name__)
@@ -20,4 +21,5 @@ def ox():
   return render_template("marubatsu.html")
 
 if __name__ == "__main__":
-    app.run()
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host='0.0.0.0', port=port)
